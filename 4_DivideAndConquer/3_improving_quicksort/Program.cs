97485dc239e8f6d179890a7e10ec40803cb4fc36
alshknv <al.shikunov81@gmail.com>
2021-08-03 20:11:16 +0300

    improving quicksort

--- 4_DivideAndConquer/3_improving_quicksort/Program.cs
+++ 4_DivideAndConquer/3_improving_quicksort/Program.cs
@@ -1,12 +1,92 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace _3_improving_quicksort
 {
-    static class Program
+    public static class ImprovingQuicksort
     {
+        private readonly static Random random = new Random();
+
+        private static void Swap(long[] array, int idx1, int idx2)
+        {
+            var buf = array[idx1];
+            array[idx1] = array[idx2];
+            array[idx2] = buf;
+        }
+
+        private static int[] Partition3(long[] a, int l, int r)
+        {
+            int[] m = new int[2] { l, l };
+            long x = a[l];
+            for (int i = l + 1; i <= r; i++)
+            {
+                if (a[i] <= x)
+                {
+                    m[1]++;
+                    Swap(a, i, m[1]);
+                    if (a[m[1]] < x)
+                    {
+                        Swap(a, m[0], m[1]);
+                        m[0]++;
+                    }
+                }
+            }
+            return m;
+        }
+
+        private static int Partition2(long[] a, int l, int r)
+        {
+            long x = a[l];
+            int j = l;
+            for (int i = l + 1; i <= r; i++)
+            {
+                if (a[i] <= x)
+                {
+                    j++;
+                    Swap(a, i, j);
+                }
+            }
+            Swap(a, l, j);
+            return j;
+        }
+
+        private static void RandomizedQuickSort2(long[] a, int l, int r)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+            int k = random.Next(r - l) + l;
+            Swap(a, l, k);
+            int m = Partition2(a, l, r);
+            RandomizedQuickSort2(a, l, m - 1);
+            RandomizedQuickSort2(a, m + 1, r);
+        }
+
+        private static void RandomizedQuickSort3(long[] a, int l, int r)
+        {
+            if (l >= r)
+            {
+                return;
+            }
+            int k = random.Next(r - l) + l;
+            Swap(a, l, k);
+            int[] m = Partition3(a, l, r);
+            RandomizedQuickSort3(a, l, m[0] - 1);
+            RandomizedQuickSort3(a, m[1] + 1, r);
+        }
+
+        public static string Solve(string input)
+        {
+            var array = input.Split(' ').Select(x => long.Parse(x)).ToArray();
+            RandomizedQuickSort3(array, 0, array.Length - 1);
+            return string.Join(" ", array);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            var input = Console.ReadLine();
+            Console.WriteLine(Solve(input));
         }
     }
 }
--- 4_DivideAndConquer/Tests/3_ImprovingQuicksortTests.cs
+++ 4_DivideAndConquer/Tests/3_ImprovingQuicksortTests.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Linq;
 using System;
 using Xunit;
 using _3_improving_quicksort;
@@ -9,7 +11,27 @@ namespace Tests
         [Fact]
         public void Test1()
         {
+            var result = ImprovingQuicksort.Solve("2 3 9 2 2");
+            Assert.Equal("2 2 2 3 9", result);
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            var result = ImprovingQuicksort.Solve("2 8 3 6 1 2 9 2 0 2");
+            Assert.Equal("0 1 2 2 2 2 3 6 8 9", result);
+        }
+
+        [Fact]
+        public void Stress()
+        {
+            var input = $"0 2 1 {string.Join(" ", Enumerable.Repeat(3, 100000).ToArray())} 5 4";
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = ImprovingQuicksort.Solve(input);
+            watch.Stop();
+            Assert.Equal($"0 1 2 {string.Join(" ", Enumerable.Repeat(3, 100000).ToArray())} 4 5", result);
+            Assert.True(watch.ElapsedMilliseconds < 500);
         }
     }
 }
