52560b364be9cf00f75d9dbc343fd70e5e430d18
alshknv <al.shikunov81@gmail.com>
2021-11-02 18:55:04 +0300

    +

--- 5_AdvancedAlgorithmsAndComplexity/week1_FlowsInNetworks/1_evacuation/Program.cs
+++ 5_AdvancedAlgorithmsAndComplexity/week1_FlowsInNetworks/1_evacuation/Program.cs
@@ -6,13 +6,12 @@ namespace _1_evacuation
 {
     public class Edge
     {
-        public int Capacity;
         public int Flow;
         public int Source;
         public int Destination;
     }
 
-    public class ResidualEdge
+    public class TwoWayEdge
     {
         public Edge EdgeF;
         public Edge EdgeR;
@@ -20,35 +19,22 @@ namespace _1_evacuation
 
     public class Node
     {
-        public List<Edge> Edges = new List<Edge>();
-    }
-
-    public class ResidualNode
-    {
         public bool Visited;
-        public ResidualEdge PathBack;
-        public List<ResidualEdge> Edges = new List<ResidualEdge>();
+        public TwoWayEdge PathBack;
+        public List<TwoWayEdge> Edges = new List<TwoWayEdge>();
     }
-
-    /*public class PathSegment
-    {
-        public int Source;
-        public int Destination;
-        public int Edge1;
-        public int Edge2;
-        public int Capacity;
-    }*/
 
     public class Path
     {
-        public ResidualEdge[] Edges;
+        public TwoWayEdge[] Edges;
         public int MinCapacity;
     }
 
     public static class Evacuation
     {
-        private static Path FindPath(ResidualNode[] residual)
+        private static Path FindPath(Node[] residual)
         {
+            //breadth-first search
             var queue = new Queue<int>();
             queue.Enqueue(1);
             Path result = null;
@@ -58,7 +44,8 @@ namespace _1_evacuation
                 var i = queue.Dequeue();
                 if (i == residual.Length - 1)
                 {
-                    var pathEdges = new Stack<ResidualEdge>();
+                    // reconstruct path if sink was found
+                    var pathEdges = new Stack<TwoWayEdge>();
                     var minCapacity = int.MaxValue;
                     while (i > 1)
                     {
@@ -75,17 +62,18 @@ namespace _1_evacuation
                 }
                 for (int j = 0; j < residual[i].Edges.Count; j++)
                 {
+                    // add children with positive flows that weren't visited yet to queue
                     if (residual[i].Edges[j].EdgeF.Flow <= 0) continue;
                     var next = residual[i].Edges[j].EdgeF.Destination;
-                    if (!residual[next].Visited)
-                    {
-                        touched.Add(next);
-                        residual[next].Visited = true;
-                        residual[next].PathBack = residual[i].Edges[j];
-                        queue.Enqueue(next);
-                    }
+                    if (residual[next].Visited) continue;
+                    touched.Add(next);
+                    residual[next].Visited = true;
+                    residual[next].PathBack = residual[i].Edges[j];
+                    queue.Enqueue(next);
                 }
             }
+
+            // restore after search
             foreach (var t in touched)
             {
                 residual[t].Visited = false;
@@ -96,35 +84,33 @@ namespace _1_evacuation
 
         public static string Solve(string[] input)
         {
+            // initialization
             var nmCount = input[0].Split(' ').Select(x => int.Parse(x)).ToArray();
             var network = new Node[nmCount[0] + 1];
-            var residual = new ResidualNode[nmCount[0] + 1];
             for (int i = 1; i <= nmCount[0]; i++)
             {
                 network[i] = new Node();
-                residual[i] = new ResidualNode();
             }
 
             for (int j = 1; j <= nmCount[1]; j++)
             {
                 var edge = input[j].Split(' ').Select(x => int.Parse(x)).ToArray();
-                network[edge[0]].Edges.Add(new Edge() { Destination = edge[1], Capacity = edge[2] });
                 var edgeF = new Edge() { Destination = edge[1], Source = edge[0], Flow = edge[2] };
                 var edgeR = new Edge() { Destination = edge[0], Source = edge[1], Flow = 0 };
-                residual[edge[0]].Edges.Add(new ResidualEdge() { EdgeF = edgeF, EdgeR = edgeR });
-                residual[edge[1]].Edges.Add(new ResidualEdge() { EdgeF = edgeR, EdgeR = edgeF });
+                network[edge[0]].Edges.Add(new TwoWayEdge() { EdgeF = edgeF, EdgeR = edgeR });
+                network[edge[1]].Edges.Add(new TwoWayEdge() { EdgeF = edgeR, EdgeR = edgeF });
             }
 
+            // Edmonds-Karp algorithm
             var flow = 0;
             while (true)
             {
-                var path = FindPath(residual);
+                var path = FindPath(network);
                 if (path == null) return flow.ToString();
                 for (int i = 0; i < path.Edges.Length; i++)
                 {
-                    var e = path.Edges[i];
-                    e.EdgeF.Flow -= path.MinCapacity;
-                    e.EdgeR.Flow += path.MinCapacity;
+                    path.Edges[i].EdgeF.Flow -= path.MinCapacity;
+                    path.Edges[i].EdgeR.Flow += path.MinCapacity;
                 }
                 flow += path.MinCapacity;
             }
