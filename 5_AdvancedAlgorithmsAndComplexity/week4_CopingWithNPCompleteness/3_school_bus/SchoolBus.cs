d360429cb107dc1620330535b79deb1bbca6aca1
alshknv <al.shikunov81@gmail.com>
2021-11-26 12:33:15 +0300

    problem 3

--- 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/3_school_bus/SchoolBus.cs
+++ 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/3_school_bus/SchoolBus.cs
@@ -39,10 +39,11 @@ namespace _3_school_bus
             return set.ToArray();
         }
 
-        private static void TSP(int n, int[,] matrix)
+        private static string[] TSP(int n, int[,] matrix)
         {
             var c = new int[(int)Math.Pow(2, n)];
             var prev = new int[n];
+            var included = new bool[n + 1];
             for (int k = 2; k <= n; k++)
             {
                 foreach (var setCur in SetsNk(n, k))
@@ -50,23 +51,34 @@ namespace _3_school_bus
                     c[setCur.Key] = int.MaxValue;
                     for (int i = 0; i < setCur.Value.Length; i++)
                     {
-                        var si = setCur.Key ^ (1 << setCur.Value[i]);
-                        foreach (var setPrev in SetBits(si))
+                        var prevValue = setCur.Key ^ (1 << setCur.Value[i]);
+                        foreach (var setPrev in SetBits(prevValue))
                         {
-                            var len = c[si] + matrix[setCur.Value[i] + 1, setPrev + 1];
-                            if (len < c[setCur.Key])
+                            var d = c[prevValue] + matrix[setCur.Value[i] + 1, setPrev + 1];
+                            if (d < c[setCur.Key])
                             {
-                                c[setCur.Key] = len;
-                                prev[setCur.Value[i]] = setPrev + 1;
+                                c[setCur.Key] = d;
+                                prev[n - setCur.Value[i] - 1] = setPrev + 1;
+                                included[setPrev + 1] = true;
                             }
                         }
                     }
                 }
             }
-            foreach (var setk in SetsNk(n, n))
-            {
 
+            for (int i = 1; i <= included.Length; i++)
+            {
+                if (!included[i])
+                {
+                    prev[0] = i;
+                    break;
+                }
             }
+
+            return new string[] {
+                (c[c.Length - 1] + matrix[prev[0], prev[1]]).ToString(),
+                string.Join(" ", prev)
+            };
         }
 
         public static string[] Solve(string[] input)
@@ -78,21 +90,11 @@ namespace _3_school_bus
                 var edge = input[i].Split(' ').Select(x => int.Parse(x)).ToArray();
                 matrix[edge[0], edge[1]] = matrix[edge[1], edge[0]] = edge[2];
             }
-            TSP(nm[0], matrix);
-            return new string[0];
+            return TSP(nm[0], matrix);
         }
 
         static void Main(string[] args)
         {
-            var gg = Solve(new string[] {"4 6",
-"1 2 20",
-"1 3 42",
-"1 4 35",
-"2 3 30",
-"2 4 34",
-"3 4 12"});
-            return;
-
             var nmline = Console.ReadLine();
             var m = int.Parse(nmline.Split(' ').Last());
             var input = new string[m + 1];
