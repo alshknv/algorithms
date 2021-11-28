45f0414e649ef8d0f6d39f8ad4b06a60db629d86
alshknv <al.shikunov81@gmail.com>
2021-11-28 12:46:41 +0300

    +

--- 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/3_school_bus/SchoolBus.cs
+++ 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/3_school_bus/SchoolBus.cs
@@ -41,52 +41,100 @@ namespace _3_school_bus
 
         private static string[] TSP(int n, int[,] matrix)
         {
-            var c = new int[(int)Math.Pow(2, n)];
-            var prev = new int[n];
-            var included = new bool[n + 1];
+            var n2 = (int)Math.Pow(2, n);
+            var c = new int[n2][];
+            var prev = new int[n2][];
+
+            foreach (var setInit in SetsNk(n, 1))
+            {
+                prev[setInit.Key] = new int[2] { int.MaxValue, -1 };
+                c[setInit.Key] = new int[n + 1];
+            }
+
             for (int k = 2; k <= n; k++)
             {
                 foreach (var setCur in SetsNk(n, k))
                 {
-                    c[setCur.Key] = int.MaxValue;
+                    c[setCur.Key] = new int[n];
+                    prev[setCur.Key] = new int[2] { int.MaxValue, -1 };
+                    c[setCur.Key][0] = int.MaxValue;
                     for (int i = 0; i < setCur.Value.Length; i++)
                     {
-                        var prevValue = setCur.Key ^ (1 << setCur.Value[i]);
-                        foreach (var setPrev in SetBits(prevValue))
+                        if (setCur.Value[i] == 0) continue;
+                        c[setCur.Key][setCur.Value[i]] = int.MaxValue;
+                        for (int j = 0; j < setCur.Value.Length; j++)
                         {
-                            var d = c[prevValue] + matrix[setCur.Value[i] + 1, setPrev + 1];
-                            if (d < c[setCur.Key])
+                            if (setCur.Value[i] != setCur.Value[j] && matrix[setCur.Value[i] + 1, setCur.Value[j] + 1] > 0)
                             {
-                                c[setCur.Key] = d;
-                                prev[n - setCur.Value[i] - 1] = setPrev + 1;
-                                included[setPrev + 1] = true;
+                                var prevValue = setCur.Key ^ (1 << setCur.Value[i]);
+                                var pathLen = c[prevValue][setCur.Value[j]] + matrix[setCur.Value[i] + 1, setCur.Value[j] + 1];
+                                if (pathLen > 0 && pathLen < c[setCur.Key][setCur.Value[i]])
+                                {
+                                    c[setCur.Key][setCur.Value[i]] = pathLen;
+                                    if (pathLen < prev[setCur.Key][0])
+                                    {
+                                        prev[setCur.Key][0] = pathLen;
+                                        prev[setCur.Key][1] = setCur.Value[i];
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
 
-            for (int i = 1; i < included.Length; i++)
+            var minPath = int.MaxValue;
+            var lastVertex = 0;
+            for (int i = 1; i < n; i++)
             {
-                if (!included[i])
+                if (matrix[i + 1, 1] > 0)
                 {
-                    prev[0] = i;
-                    break;
+                    var path = c[c.Length - 1][i] + matrix[i + 1, 1];
+                    if (path < minPath)
+                    {
+                        minPath = path;
+                        lastVertex = i;
+                    }
                 }
             }
+            prev[prev.Length - 1][0] = minPath;
+            prev[prev.Length - 1][1] = lastVertex;
 
-            if (prev[0] == 0) return new string[] { "-1" };
+            var result = new int[n];
+            var rIdx = n - 1;
+            var prevIdx = prev.Length - 1;
+            while (prev[prevIdx][1] >= 0)
+            {
+                result[rIdx--] = prev[prevIdx][1] + 1;
+                prevIdx ^= (1 << prev[prevIdx][1]);
+            }
+            result[0] = 1;
 
-            return new string[] {
-                (c[c.Length - 1] + matrix[prev[0], prev[1]]).ToString(),
-                string.Join(" ", prev)
-            };
+            if (minPath > 0 && minPath < int.MaxValue)
+            {
+                return new string[] {
+                    minPath.ToString(),
+                    string.Join(" ", result)
+                };
+            }
+            else
+            {
+                return new string[] { "-1" };
+            }
         }
 
         public static string[] Solve(string[] input)
         {
             var nm = input[0].Split(' ').Select(x => int.Parse(x)).ToArray();
             var matrix = new int[nm[0] + 1, nm[0] + 1];
+            for (int i = 0; i <= nm[0]; i++)
+            {
+                for (int j = 0; j <= nm[0]; j++)
+                {
+                    matrix[i, j] = -1;
+                }
+            }
+
             for (int i = 1; i < input.Length; i++)
             {
                 var edge = input[i].Split(' ').Select(x => int.Parse(x)).ToArray();
@@ -97,6 +145,16 @@ namespace _3_school_bus
 
         static void Main(string[] args)
         {
+            var gg = Solve(new string[] {
+                "4 6",
+                "1 2 20",
+                "1 3 42",
+                "1 4 35",
+                "2 3 30",
+                "2 4 34",
+                "3 4 12"
+            });
+            return;
             var nmline = Console.ReadLine();
             var m = int.Parse(nmline.Split(' ').Last());
             var input = new string[m + 1];
--- 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/Tests/SchoolBusTests.cs
+++ 5_AdvancedAlgorithmsAndComplexity/week4_CopingWithNPCompleteness/Tests/SchoolBusTests.cs
@@ -22,6 +22,54 @@ namespace Tests
         }
 
         [Fact]
+        public void Example2()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "4 6",
+                "1 2 1",
+                "2 3 5",
+                "1 3 1",
+                "1 4 10",
+                "2 4 2",
+                "3 4 3"
+            }),
+            l1 => Assert.Equal("7", l1),
+            l2 => Assert.Equal("1 3 4 2", l2));
+        }
+
+        [Fact]
+        public void Triangle()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "3 3",
+                "1 2 1",
+                "2 3 1",
+                "1 3 1"
+            }),
+            l1 => Assert.Equal("3", l1),
+            l2 => Assert.Equal("1 3 2", l2));
+        }
+
+        [Fact]
+        public void SingleVertex()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "1 0"
+            }),
+            l1 => Assert.Equal("-1", l1));
+        }
+
+        [Fact]
+        public void TwoVertices()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "2 1",
+                "1 2 1"
+            }),
+            l1 => Assert.Equal("-1", l1));
+        }
+
+        [Fact]
         public void NoPath()
         {
             Assert.Collection(SchoolBus.Solve(new string[] {
@@ -32,6 +80,67 @@ namespace Tests
                 "4 2 1"
             }),
             l1 => Assert.Equal("-1", l1));
+        }
+
+        [Fact]
+        public void ExtraTest1()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "4 4",
+                "1 3 6",
+                "1 4 2",
+                "2 3 6",
+                "2 4 2"
+            }),
+            l1 => Assert.Equal("16", l1),
+            l2 => Assert.Equal("1 3 2 4", l2)); // 1 4 2 3
+        }
+
+        [Fact]
+        public void ExtraTest2()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "4 4",
+                "1 2 1",
+                "1 4 2",
+                "2 3 2",
+                "3 4 6"
+            }),
+            l1 => Assert.Equal("11", l1),
+            l2 => Assert.Equal("1 2 3 4", l2)); // 1 4 3 2
+        }
+
+        [Fact]
+        public void ExtraTest3()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "5 6",
+                "1 2 3",
+                "1 3 1",
+                "1 5 5",
+                "2 5 9",
+                "3 4 8",
+                "4 5 5"
+            }),
+            l1 => Assert.Equal("26", l1),
+            l2 => Assert.Equal("1 2 5 4 3", l2)); // 1 3 4 5 2
+        }
+
+        [Fact]
+        public void ExtraTest4()
+        {
+            Assert.Collection(SchoolBus.Solve(new string[] {
+                "5 7",
+                "1 2 1",
+                "1 4 9",
+                "2 4 1",
+                "2 5 9",
+                "3 4 6",
+                "3 5 8",
+                "4 5 1"
+            }),
+            l1 => Assert.Equal("33", l1),
+            l2 => Assert.Equal("1 2 5 3 4", l2)); // 1 4 3 5 2
         }
     }
 }
