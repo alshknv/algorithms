fd2678bdb8f92b49bd1666c43ffa04c63702d0bc
alshknv <al.shikunov81@gmail.com>
2021-12-11 17:09:51 +0300

    init

---
+++ 6_GenomeAssembling/week1_OverlapGraphs/1_phiX174_error_free/ErrorFree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace _1_phiX174_error_free
+{
+    public static class ErrorFree
+    {
+        public static string Assemble(string[] reads)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            var reads = new string[1618];
+            for (int i = 0; i < 1618; i++)
+                reads[i] = Console.ReadLine();
+            Console.WriteLine(Assemble(reads));
+        }
+    }
+}
